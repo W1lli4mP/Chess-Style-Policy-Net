d4e5311b3ae4b01ae749a97d470ae53b9d50ccf1
EE <williamencabopimentel@gmail.com>
2026-06-08 21:26:29 +0100

    Add data cleaning diagnostics + Simplify dataframe column handling

--- src/build_dataset.py
+++ src/build_dataset.py
@@ -1,9 +1,5 @@
 import json
-
 import pandas as pd
-import pyarrow as pa
-import pyarrow.parquet as pq
-
 from pathlib import Path
 
 from config import USERNAMES
@@ -39,7 +35,176 @@ def load_user_games(username: str) -> list[dict]:
     
     return all_games
 
-def clean_data() -> None:
+def clean_games_dataframe(df: pd.DataFrame, diagnostics: bool = False) -> pd.DataFrame:
+    df = df.copy()
+
+    # flatten nested player dicts
+    # df["white"] and df["black"] are series' so extracting their fields and adding a prefix
+    # allows their fields to be exposed and access more easily
+    white_df = pd.json_normalize(df["white"]).add_prefix("white_")
+    black_df = pd.json_normalize(df["black"]).add_prefix("black_")
+
+    # remove old series
+    df = pd.concat(
+        [df.drop(columns=["white", "black"]), white_df, black_df],
+        axis=1
+    )
+
+    # normalise usernames to be in lowercase
+    usernames = { u.lower() for u in USERNAMES }
+
+    df["white_username"] = df["white_username"].astype("string").str.lower()
+    df["black_username"] = df["black_username"].astype("string").str.lower()
+
+    required_columns = [
+        "pgn",
+        "time_control",
+        "end_time",
+        "rated",
+        "uuid",
+        "initial_setup",
+        "fen",
+        "rules",
+        "time_class",
+        "white_rating",
+        "white_result",
+        "white_username",
+        "black_rating",
+        "black_result",
+        "black_username"
+    ]
+
+    # partition each condition to diagnose individually
+
+    valid_required = df[required_columns].notna().all(axis=1)
+
+    valid_time_class = df["time_class"].isin(["bullet", "blitz", "rapid"])
+
+    valid_rules = df["rules"].eq("chess")
+
+    valid_username = (
+        df["white_username"].isin(usernames) |
+        df["black_username"].isin(usernames)
+    )
+
+    mask = (
+        valid_required &
+        valid_time_class &
+        valid_rules &
+        valid_username
+    )
+
+    #! optional diagnostics for tracking why games were dropped
+    if diagnostics:
+        print("Missing required value:", (~valid_required).sum())
+        print("Invalid time class:", (~valid_time_class).sum())
+        print("Rules not chess:", (~valid_rules).sum())
+        print("Username not recognised:", (~valid_username).sum())
+
+        # restrict diagnostics to games with the correct time control
+        # NOTE: change for testing outside time control
+        target_class = valid_time_class
+
+        print(
+            "Target games missing required values:",
+            (target_class & ~valid_required).sum(),
+        )
+
+        print(
+            "Target games with non-chess rules:",
+            (target_class & ~valid_rules).sum(),
+        )
+
+        print(
+            "Target games with unrecognised usernames:",
+            (target_class & ~valid_username).sum(),
+        )
+
+        #* populate diagnostic df with boolean algebra
+        diagnostic_df = df.loc[target_class].copy()
+
+        diagnostic_df["missing_required"] = (
+            ~valid_required.loc[target_class]
+        )
+        diagnostic_df["invalid_rules"] = (
+            ~valid_rules.loc[target_class]
+        )
+        diagnostic_df["username_not_recognised"] = (
+            ~valid_username.loc[target_class]
+        )
+
+        failure_columns = [
+            "missing_required",
+            "invalid_rules",
+            "username_not_recognised"
+        ]
+
+        failed = diagnostic_df.loc[
+            diagnostic_df[failure_columns].any(axis=1)
+        ]
+
+        print("\nREJECTED BULLET, BLITZ AND RAPID GAMES:")
+
+        print(
+            failed[
+                [
+                    "uuid",
+                    "source_username",
+                    "time_class",
+                    "rules",
+                    "white_username",
+                    "black_username",
+                    "missing_required",
+                    "invalid_rules",
+                    "username_not_recognised",
+                ]
+            ].to_string(index=False)
+        )
+
+        missing_counts = (
+            df.loc[target_class, required_columns]
+                .isna()
+                .sum()
+                .sort_values(ascending=False)
+        )
+
+        print("\nMissing required fields:")
+        print(missing_counts[missing_counts > 0])
+
+    # update df with the mask
+    df = df.loc[mask].copy()
+
+    # add useful fields to help processing parquets later
+    is_white = df["white_username"].isin(usernames)
+
+    df["my_colour"] = is_white.map({
+        True: "white",
+        False: "black"
+    })
+
+    df["my_username"] = df["white_username"].where(
+        is_white,
+        df["black_username"]
+    )
+
+    df["my_rating"] = df["white_rating"].where(
+        is_white,
+        df["black_rating"]
+    )
+
+    df["opponent_rating"] = df["black_rating"].where(
+        is_white,
+        df["white_rating"]
+    )
+
+    df["rating_diff"] = df["my_rating"] - df["opponent_rating"]
+
+    df = df.drop_duplicates(subset=["source_username", "uuid"])
+    df = df.sort_values(["source_username", "end_time"])
+
+    return df.reset_index(drop=True)
+
+def build_games_dataset() -> None:
     all_games = []
 
     for username in USERNAMES:
@@ -52,30 +217,15 @@ def clean_data() -> None:
     print(df.columns)
     print(f"Loaded {len(df)} total games")
 
-    ## testing
-    print(f'There are {len(df[df["time_class"] == "bullet"])} bullet games')
-    print(f'There are {len(df[df["time_class"] == "blitz"])} blitz games')
-    print(f'There are {len(df[df["time_class"] == "rapid"])} rapid games')
-    print(f'There are {len(df[df["time_class"] == "daily"])} daily games')
-
-    print(f'Sum: {len(df[df["time_class"] == "bullet"]) + len(df[df["time_class"] == "blitz"]) + len(df[df["time_class"] == "rapid"]) + len(df[df["time_class"] == "daily"])}')
-
-
-    """
-    dataframe -> arrow table -> parquet
-    
-    example
-    df = pd.DataFrame(...)
-    table = pa.Table.from_pandas(df)
-    pq.write_table(table, "example.parquet")
-    """
-
     ## actually filter/clean the data here...
+    df_clean = clean_games_dataframe(df)
+
+    print("\nRemaining games:", len(df_clean))
 
     output_path = PROCESSED_DIR / "games_raw.parquet"
-    df.to_parquet(output_path, index=False)
+    df_clean.to_parquet(output_path, index=False)
 
     print(f"Saved {output_path}")
 
 if __name__ == "__main__":
-    clean_data()
+    build_games_dataset()
