6627d76982549614bb69d2ac95a8207994806596
EE <williamencabopimentel@gmail.com>
2026-06-28 14:22:03 +0100

    Fix top-level encoding function to handle singular boards

--- src/board_encoder.py
+++ src/board_encoder.py
@@ -5,33 +5,72 @@ import pandas as pd
 import chess
 import torch
 
-def encode_board():
+#! implement custom pytorch dataset class to use leverage batch encoding
+def encode_boards():
     training_rows = pd.read_parquet("../data/processed/training_positions.parquet")
 
     for _, training_row in training_rows.iterrows():
         fen_str = training_row["fen_before_move"]
 
         son = chess.Board(fen_str)
+        encode_board(son)
 
-        #* PIECE INFO
+def encode_board(board: chess.Board):
+    piece_info_planes = encode_piece_info(board) # (12, 8, 8)
+    
+    game_info_planes = encode_game_info(board) # (6, 8, 8)
 
-        #! ...
-        piece_info_planes = None
+    encoded_board = torch.cat(
+        [piece_info_planes, game_info_planes],
+        dim=0
+    )
 
-        #* GAME INFO
-        game_info_planes = encode_game_info(son)
+    # validate encoded board dimensions
+    assert encode_board.shape == (18, 8, 8)
 
-    # ...
+    return encoded_board
 
-
-    # return tensor x where
     # BSx18x8x8
     # x = (batch_size, channels, height, width)
 
+# 12 planes representing the piece info (12x8x8)
+def encode_piece_info(board: chess.Board) -> torch.Tensor:
+    """
+    plan
+    initialise all piece planes
+    iterate through board until piece_count() == 0
+    update while iterating
+    """
+
+    planes = torch.zeros((6, 8, 8), dtype=torch.float32)
+
+    # composite key of piece type and piece colour
+    piece_to_channel = {
+        (chess.PAWN, chess.WHITE): 0,
+        (chess.KNIGHT, chess.WHITE): 1,
+        (chess.BISHOP, chess.WHITE): 2,
+        (chess.ROOK, chess.WHITE): 3,
+        (chess.QUEEN, chess.WHITE): 4,
+        (chess.KING, chess.WHITE): 5,
+
+        (chess.PAWN, chess.BLACK): 6,
+        (chess.KNIGHT, chess.BLACK): 7,
+        (chess.BISHOP, chess.BLACK): 8,
+        (chess.ROOK, chess.BLACK): 9,
+        (chess.QUEEN, chess.BLACK): 10,
+        (chess.KING, chess.BLACK): 11,
+    }
+
+    for square, piece in board.piece_map().items():
+        channel = piece_to_channel[(piece.piece_type, piece.color)]
+        row = chess.square_rank(square)
+        col = chess.square_file(square)
+        planes[channel][row][col] = 1.0
+    
+    return planes
+
 # 6 planes representing the game info (6x8x8)
 def encode_game_info(board: chess.Board) -> torch.Tensor:
-    
-
     enc_side_to_move = torch.full(
         (8, 8),
         fill_value=float(board.turn == chess.WHITE),
