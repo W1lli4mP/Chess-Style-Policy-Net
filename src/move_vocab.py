70d3490e0ce58fbce21747ef5d39a66447f6eb83
EE <williamencabopimentel@gmail.com>
2026-06-29 00:44:49 +0100

    Add move vocabulary build

--- src/move_vocab.py
+++ src/move_vocab.py
@@ -22,12 +22,64 @@ ways to promote:
 thus, the vocab would contain 4032 + 176 = 4208 moves
 """
 
-def build_move_vocabulary():
-    pass
+import chess
 
-def move_to_id(uci: str) -> int:
-    pass
+PROMOTION_PIECES = (
+    chess.KNIGHT,
+    chess.BISHOP,
+    chess.ROOK,
+    chess.QUEEN
+)
 
-def id_to_move(id: int) -> str:
-    pass
+# move_to_id, moves
+def build_move_vocabulary() -> tuple[dict[str, int], list[str]]:
+    # construct list of uci moves
+    moves = []
 
+    for from_square in chess.SQUARES:
+        for to_square in chess.SQUARES:
+            if from_square == to_square:
+                continue
+            
+            move = chess.Move(
+                from_square=from_square,
+                to_square=to_square
+            )
+            moves.append(move.uci())
+    
+    promotion_ranks = (
+        # white: rank 7 to rank 8
+        (6, 7),
+
+        # black: rank 2 to rank 1
+        (1, 0)
+    )
+
+    for source_rank, destination_rank in promotion_ranks:
+        for source_file in range(8):
+            for file_offset in (-1, 0, 1):
+                # left, forward, right
+
+                destination_file = source_file + file_offset
+
+                if not 0 <= destination_file < 8:
+                    continue
+                
+                for promotion_piece in PROMOTION_PIECES:
+                    move = chess.Move(
+                        from_square=from_square,
+                        to_square=to_square,
+                        promotion=promotion_piece
+                    )
+                    moves.append(move.uci())
+    
+    move_to_id = {
+        uci_move: move_id for move_id, uci_move in enumerate(moves)
+    }
+
+    return move_to_id, moves
+
+if __name__ == "__main__":
+    # test
+    x, y = build_move_vocabulary()
+    print(len(x), len(y))
