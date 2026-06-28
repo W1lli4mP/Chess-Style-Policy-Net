726b76c10497246346d04d3cfa7a2732a6b5fb29
EE <williamencabopimentel@gmail.com>
2026-06-28 12:46:54 +0100

    Fix inconsistencies, redundancies and parameters

--- src/resnet_cnn.py
+++ src/resnet_cnn.py
@@ -13,16 +13,26 @@ chess_nn.forward(x) for inference
 class ChessResNet(nn.Module):
     def __init__(
         self,
+        in_channels: int = 18,
         hidden_channels: int = 64,
-        num_res_blocks: int = 4
+        num_res_blocks: int = 4,
+        board_embedding_dim: int = 256
     ):
         super().__init__()
 
         # input board tensor into projection layer before feeding into res blocks
-        #? dimensionality expansion (18 -> 64)
+        #? dimensionality expansion
+        # (batch_size, 18, 8, 8) -> (batch_size, 64, 8, 8)
         self.projection = nn.Sequential(
-            nn.Conv2d(kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=hidden_channels,
+                kernel_size=1,
+                stride=1,
+                padding=0,
+                bias=False
+            ),
+            nn.BatchNorm2d(hidden_channels),
             nn.ReLU(inplace=True)
         )
 
@@ -33,23 +43,16 @@ class ChessResNet(nn.Module):
         )
 
         # fc 256
+        # (batch_size, 64, 8, 8) -> (batch_size, 4096) -> (batch_size, 256)
         self.fc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(hidden_channels * 8 * 8, 256),
+            nn.Flatten(start_dim=1),
+            nn.Linear(hidden_channels * 8 * 8, board_embedding_dim),
             nn.ReLU(inplace=True)
         )
-
-        self.linear = nn.Linear(hidden_channels * 8 * 8, 256)
-
-        self.relu = nn.ReLU(inplace=True)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         self.projection(x)
         self.res_blocks(x)
-        
-        # flatten 4096 (from 64x8x8)
-        x = torch.flatten(x, start_dim=1)
-        x = self.linear(x)
-        x = self.relu(x)
+        self.fc(x)
         
         return x
